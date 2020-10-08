ed3dc043aa08d0364c4212a8f5d8593d60ca4ca0
JavierPM1995 <jaavierpm1995@gmail.com>
2020-10-08 03:41:20 -0300

    Fix Add user

--- src/App.js
+++ src/App.js
@@ -1,76 +1,86 @@
-import React, {useState} from 'react';
-import {v4 as uuidv4} from 'uuid';
-import UserTable from './components/UserTable';
-import AddUserForm from './components/AddUserForm';
-
-
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import UserTable from "./components/UserTable";
+import AddUserForm from "./components/AddUserForm";
 
 function App() {
-
   const usersData = [
-    {id: uuidv4(), name: 'Tania', username: 'floppydiskette'},
-    {id: uuidv4(), name: 'Craig', username: 'siliconeidolon'},
-    {id: uuidv4(), name: 'Ben', username: 'benisphere'}
-  ]
-
+    { id: uuidv4(), name: "Tania", username: "floppydiskette" },
+    { id: uuidv4(), name: "Craig", username: "siliconeidolon" },
+    { id: uuidv4(), name: "Ben", username: "benisphere" },
+  ];
 
   //State Value
-const [users, setUsers] = useState(usersData);
+  const [users, setUsers] = useState(usersData);
 
-//State Edit
-const [editingForm, setEditingForm] = useState(false)
-const [currentUser, setCurrentUser] = useState([
-  {id: null, name: '', username: ''}
-])
+  //State Edit
+  const userIniSta = { id: null, name: "", username: "" }
+  const [editingForm, setEditingForm] = useState(false);
+  const [currentUser, setCurrentUser] = useState([userIniSta]);
 
+  //Add users
+  const addUser = (user) => {
+    user.id = uuidv4();
+    setUsers([...users, user]);
+    openWindow();
+  };
 
-//Add users
-const addUser = (user) => {
-  user.id = uuidv4()
-  setUsers([
-    ...users,
-    user
-  ])
-}
+  // Delete Users
+  const deleteUser = (id) => {
+    let result = window.confirm("Are you sure do you want to erase this user?");
+    if (result === true) {
+      setUsers(users.filter((user) => user.id !== id));
+    } else {
+      return;
+    }
+  };
 
-// Delete Users
-const deleteUser = (id) => {
-  let result = window.confirm('Are you sure do you want to erase this user?')
-  if (result === true) {
-    setUsers(users.filter(user => user.id !== id));
-  } else {
-    return
-  }  
-}
+  // Edit Users
+  const editRow = (user) => {
+    setEditingForm(true);
+    setCurrentUser({
+      id: user.id,
+      name: user.name,
+      username: user.username,
+    });
+    openWindow();
+  };
 
-// Edit Users
-const editRow = (user) => {
-  setEditingForm(true);
-  setCurrentUser({
-  id: user.id, name: user.name, username: user.username
-});
-}
+  const updateUser = (id, updateUser) => {
+    setUsers(users.map((user) => (user.id === id ? updateUser : user)));
+    setCurrentUser([userIniSta])
+  };
 
-const updateUser = (id, updateUser) => {
-  setUsers(users.map(user => 
-    (user.id === id ? updateUser : user)))
-}
+  //Open Modal
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openWindow = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-      <div className="container">
-        <h1 className="my-4">CRUD App</h1>
-        <div className="row">
-          <div className="col-md-4">
-            <h2>Add users</h2>
-            <AddUserForm addUser={addUser} currentUser={currentUser} editingForm={editingForm} updateUser={updateUser} />
-          </div>
-          <div className="col-md-8">
-            <h2>View user</h2>
-            <UserTable users={users} deleteUser={deleteUser} editRow={editRow} />
-          </div>
+    <div className="container">
+      <h1 className="my-4">CRUD App</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <h2>Add users</h2>
+          <AddUserForm
+            addUser={addUser}
+            currentUser={currentUser}
+            editingForm={editingForm}
+            setEditingForm={setEditingForm}
+            updateUser={updateUser}
+            openWindow={openWindow}
+            isOpen={isOpen}
+          />
+        </div>
+        <div className="col-md-8">
+          <h2>View user</h2>
+          <UserTable users={users} deleteUser={deleteUser} editRow={editRow} />
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -1,48 +1,75 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const AddUserForm = (props) => {
-    
     //React Hook Form
-    const {register, errors, handleSubmit, setValue} = useForm({ defaultValues: props.currentUser });
+  const { register, errors, setValue, handleSubmit } = useForm({
+    defaultValues: props.currentUser
+  });
 
-    setValue('name', props.currentUser.name);
-    setValue('username', props.currentUser.username);
+  setValue("name", props.currentUser.name);
+  setValue("username", props.currentUser.username);
 
-    //Data capture
-    const onSubmit = (data, e) => {
-        if (props.editingForm === false) {
-            props.addUser(data);
-            e.target.reset();
-        } else  {
+  //Add or Edit functions
+  const onSubmit = (data) => {
+      if (props.editingForm === false) {
+          props.addUser(data);
+        } else {
             props.updateUser(props.currentUser.id, data);
-            e.target.reset();
         }
-    }
-    
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} >
-            <label htmlFor="name">Name</label>
-            <input className="control-form d-block" type="text" name="name" placeholder="Dave" ref={
-                register({ required: {value: true, message: 'Required field' } })
-            }/>
-            <div className="text-danger">
-                {errors?.name?.message}
-            </div>
-            <label htmlFor="username">Username</label>
-            <input className="control-form d-block" type="text" name="username" placeholder="Dave01" ref={
-                register({ required: { value: true, message: "Required field" }})
-            } /> 
-            <div className="text-danger" >
-                {errors?.username?.message}
-            </div>
-            <button className="btn btn-primary my-3">
-                {
-                    props.editingForm ? ( 'Edit user' ) : ( 'Add user' )
-                }
-            </button>
-        </form>
-    )
-}
+        props.openWindow()
+        props.setEditingForm(false)
+  };
+
+  return (
+    <div>
+        <button className="btn btn-primary" onClick={props.openWindow} >Add a new user</button>
+        <Modal isOpen={props.isOpen}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ModalHeader>
+          {props.editingForm ? (
+            <h3>What do you like to Modified?</h3>
+          ) : (
+            <h3>What new user do you like to create?</h3>
+          )}
+        </ModalHeader>
+        <ModalBody className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            className="form-control d-block mb-1"
+            type="text"
+            name="name"
+            placeholder="My name"
+            ref={register({
+              required: { value: true, message: "Required field" },
+            })}
+          />
+          <div className="text-danger">{errors?.name?.message}</div>
+          <label htmlFor="username">Username</label>
+          <input
+            className="form-control d-block"
+            type="text"
+            name="username"
+            placeholder="My username"
+            ref={register({
+              required: { value: true, message: "Required field" },
+            })}
+          />
+          <div className="text-danger">{errors?.username?.message}</div>
+        </ModalBody>
+        <ModalFooter>
+            {
+            props.editingForm ? 
+            <button className="btn btn-primary">"Edit user"</button>
+             : 
+             <button className="btn btn-primary">"Add new user"</button>
+            }
+        </ModalFooter>
+      </form>
+    </Modal>
+    </div>
+  );
+};
 
 export default AddUserForm;
